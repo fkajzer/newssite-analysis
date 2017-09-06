fe6662266d01e1ceb310c335ab5a41ee575e6898
Filip Kajzer <fkajzer@Filips-MacBook-Pro.local>
2017-09-06 18:25:44 +0200

    eval.py will now iterate over all classifiers and features and stop after it is done for dataset

--- eval.py
+++ eval.py
@@ -5,6 +5,8 @@ from germannewssites.datasets.load import load, load_test
 import argparse
 import logging
 
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     LOGFMT = '%(asctime)s %(name)s %(levelname)s %(message)s'
 
@@ -14,8 +16,10 @@ if __name__ == '__main__':
                            help='Set data amount (1k, 2k, 4k, 8k, 16k)')
     argparser.add_argument('-s', '--sites', dest='sites', type=int, default=5,
                            help='Set number of sites (3, 5)')
-    argparser.add_argument('-c', '--classifier', dest='classifier', type=str, default="linear_svc",
+    argparser.add_argument('-c', '--classifier', dest='classifier', type=str, default="none",
                            help='Set the classifier to use (linear_svc, knn, random_forest)')
+    argparser.add_argument('-f', '--feature', dest='feature', type=str, default="none",
+                           help='Set the feature to use (unigram, bigram, uni-bigram, char, germannewssite)')
 
     logging.basicConfig(level=getattr(logging, 'DEBUG'), format=LOGFMT)
     args = argparser.parse_args()
@@ -25,34 +29,46 @@ if __name__ == '__main__':
     X_train, y_train = load(data_set_destination)
     X_test, y_test = load_test(args.sites)
 
-    features = ['unigram', 'bigram', 'uni-bigram', 'char', 'partofspeech', 'germannewssite']
-    #features = ['partofspeech']
+    if args.feature == "none":
+        #features = ['unigram', 'bigram', 'uni-bigram', 'char', 'partofspeech', 'germannewssite']
+        #features = ['partofspeech'] is giving ill results?
+        features = ['unigram', 'bigram', 'uni-bigram', 'char', 'germannewssite']
+    else:
+        features = [args.feature]
 
-    if args.classifier == "linear_svc":
-        hyper_parameters = {
-            'classifier__C': (0.001, 0.01, 0.1, 1.0 , 10, 100, 1000),
-        }
-    if args.classifier == "svc":
-        hyper_parameters = {
-            'classifier__C': (0.001, 1.0, 100),
-            'classifier__gamma': (0.001, 0.01, 0.1, 1.0, 10)
-        }
-    if args.classifier == "knn":
-        hyper_parameters = {
-            'classifier__n_neighbors': (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
-        }
-    if args.classifier == "random_forest":
-        hyper_parameters = {
-            "classifier__n_estimators": (10, 20, 50),
-            "classifier__max_depth": [10, 50, 100, None],
-            "classifier__min_samples_split": [2, 3, 10],
-            "classifier__min_samples_leaf": [1, 3, 10],
-        }
+    if args.classifier == "none":
+        classifiers = ['svc', 'linear_svc', 'knn', 'random_forest']
+    else:
+        classifiers = [args.classifier]
 
     #run benchmark for each feature
     benchmark = SklearnBenchmark()
-    for feature in features:
-        output_folder_name = "{}/{}/{}/{}".format(args.data_set, args.sites, args.classifier, feature)
+    for classifier in classifiers:
+        if classifier == "linear_svc":
+            hyper_parameters = {
+                'classifier__C': (0.001, 0.01, 0.1, 1.0 , 10, 100, 1000),
+            }
+        if classifier == "svc":
+            hyper_parameters = {
+                'classifier__C': (0.001, 1.0, 100),
+                'classifier__gamma': (0.001, 0.01, 0.1, 1.0, 10)
+            }
+        if classifier == "knn":
+            hyper_parameters = {
+                'classifier__n_neighbors': (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+            }
+        if classifier == "random_forest":
+            hyper_parameters = {
+                "classifier__n_estimators": (10, 20, 50),
+                "classifier__max_depth": [10, 50, 100, None],
+                "classifier__min_samples_split": [2, 3, 10],
+                "classifier__min_samples_leaf": [1, 3, 10],
+            }
 
-        profiler_instance = GermanNewssiteProfiler(method=classifier, feature=feature)
-        benchmark.run(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, profiler=profiler_instance, output_folder_name=output_folder_name, hyper_parameters=hyper_parameters)
+        for feature in features:
+
+            logger.info("Starting Benchmark for classifier: {} with feature: {}!".format(classifier, feature))
+            output_folder_name = "{}/{}/{}/{}".format(args.data_set, args.sites, classifier, feature)
+
+            profiler_instance = GermanNewssiteProfiler(method=classifier, feature=feature)
+            benchmark.run(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, profiler=profiler_instance, output_folder_name=output_folder_name, hyper_parameters=hyper_parameters)
